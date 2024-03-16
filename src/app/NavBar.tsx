9783a5793b560d438a7a955778c0e78acb3d5cd8
literate <gaekoon@gmail.com>
2024-03-16 09:35:49 +0900

    feat: 네비게이션 바를 생성한다

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 const NavBar = () => {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>NavBar</h1>
+    <div className="nav">
+      <div className="nav__offset"></div>
+      <div className="nav__home">
+        <div className="nav__logo" onClick={() => router.push("/")}>
+          Wrtnrd
+        </div>
+        <input className="nav__search" />
+        <div className="nav__items">
+          <Link href="/edit">Edit</Link>
+          <Link href="/signin">SignIn</Link>
+          <Link href="/mypage">MyPage</Link>
+        </div>
+      </div>
     </div>
   );
 };
