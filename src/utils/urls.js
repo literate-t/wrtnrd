331a260a103e9fb10eaccdbf4c14ba4114344edb
literate <gaekoon@gmail.com>
2024-05-10 23:09:03 +0900

    feat: url을 프론트엔드와 백엔드로 분리한다

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,1 +1,7 @@
-export const SIGN_IN_URL = "http://localhost:8080/api/auth/authenticate";
+// Frontend
+export const ROOT_URL = "/";
+export const SIGN_URL = "/signin";
+
+// Backend
+const baseURL = "http://localhost:8080";
+export const API_SIGN_IN = `${baseURL}/api/auth/authenticate`;
