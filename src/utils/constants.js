1b758ee53f215c6f1aef247c5ce70588b2910677
literate <gaekoon@gmail.com>
2024-05-10 23:02:28 +0900

    feat: 401 상태를 추가한다

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 export const KEY_SIGN_IN = "isSignIn";
 export const TRUE = "true";
 export const FALSE = "false";
+export const ERROR_UNAUTHENTICATED_401 = 401;
 export const ERROR_FORBIDDEN_403 = 403;
