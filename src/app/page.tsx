eeab2d50fd247409bd1d1f86221191c2e53e3026
literate <gaekoon@gmail.com>
2024-05-13 01:33:14 +0900

    feat: 테스트용 데이터를 추가한다

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import PostForm from "@/components/PostForm";
+import PostBox from "@/components/PostBox";
+
 const Home = () => {
   return (
     <main className="main">
-      <div className="main--container">Main</div>
+      <div className="main--container">
+        <PostForm />
+        <PostBox />
+      </div>
     </main>
   );
 };
---
+++ src/data/dummy.js
@@ -0,0 +1,32 @@
+export const dummyData = [
+  {
+    id: 1,
+    title: "거짓말 거짓말 거짓말",
+    author: "이적",
+    createdAt: new Date().toLocaleDateString("ko", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    })
+  },
+  {
+    id: 2,
+    title: "하늘을 달리다",
+    author: "이적",
+    createdAt: new Date().toLocaleDateString("ko", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    })
+  },
+  {
+    id: 3,
+    title: "나타나",
+    author: "김범수",
+    createdAt: new Date().toLocaleDateString("ko", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    })
+  },
+]
