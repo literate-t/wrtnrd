ebbe8bdcc6d3ad9e7541b477ef9329690e93aec2
literate <gaekoon@gmail.com>
2024-05-14 22:32:27 +0900

    feat: 별도로 분리한 곳에서 폰트 파일을 가져온다

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,20 @@
 import type { Metadata } from "next";
-import {Noto_Sans_KR} from "next/font/google";
 import NavBar from "./NavBar";
 import "@/app/index.scss";
 import TanstackProvider from "@/providers/TanstackProvider";
 import RecoilRootWrapper from "@/Wrappers/RecoilRootWrapper";
+import { notoSansKr } from "@/utils/font";
 
 export const metadata: Metadata = {
   title: "Wrtnrd",
   description: "Write and read",
 };
 
-const notoSansKr = Noto_Sans_KR({
-  weight: ['400'],
-  subsets: ['latin'],
-});
-
 const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  
   return (
     <html lang="en">
       <body className={notoSansKr.className}>
