c0d6090127231574d19a925f0657a01951e2791d
literate <gaekoon@gmail.com>
2024-05-17 11:43:44 +0900

    feat: Main 화면에서 최초에 10개의 (테스트) 데이터를 가져온다 - react-query를 활용

--- src/components/posts/PostBoxList.tsx
+++ src/components/posts/PostBoxList.tsx
@@ -1,13 +1,30 @@
+"use client";
+
 import PostBox from "@/components/posts/PostBox";
-import { data } from "@/data/dummy";
+import axios from "@/utils/axios";
+import { useQuery } from "@tanstack/react-query";
+import { PostProps } from "@/interfaces/postInterface";
+
+const fetchPosts = async () => {
+  const res = await axios.get("/api/post/list");
+  return res.data;
+};
 
 const PostBoxList = () => {
+  const { isFetching, isError, data } = useQuery<PostProps[]>({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isFetching) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error</div>;
+  }
+
   return (
-    <div>
-      {data.map((post) => (
-        <PostBox post={post} key={post.id} />
-      ))}
-    </div>
+    <div>{data?.map((post) => <PostBox post={post} key={post.id} />)}</div>
   );
 };
 
