4637668c288f9d1855e6c4fbd1f2101f0c46df17
literate <gaekoon@gmail.com>
2024-05-17 11:46:27 +0900

    feat: 포스트의 본문과 제목을 상태 변수에 담는다 - 비로그인 사용자가 포스트 저장을 하려고 하면 로그인 화면으로 보낸다 - 제목이나 본문 내용이 없으면 알림창을 띄운다

--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -1,32 +1,80 @@
 "use client";
 
 import "./index.scss";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { notoSansKr } from "@/utils/font";
+import { useRecoilValue } from "recoil";
+import { authAtoms } from "@/atoms/authAtoms";
+import { useRouter } from "next/navigation";
+import axios from "@/utils/axios";
 
 const PostForm = () => {
-  const [content, setContent] = useState<string>();
+  const [body, setBody] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const authUser = useRecoilValue(authAtoms);
+  const router = useRouter();
 
-  const handleFormTextarea = () => {};
-  const handleSubmit = () => {};
+  const handleBodyText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setBody(e.target.value);
+  };
+
+  const handleTitleText = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (null == authUser) {
+      router.push("/signin");
+      return;
+    } else if (null == body || body?.length <= 0) {
+      alert("The body is empty");
+      return;
+    } else if (null == title || title?.length <= 0) {
+      alert("The title is empty");
+      return;
+    }
+
+    axios("/api/post/new", {
+      method: "POST",
+      data: {
+        userId: authUser?.id,
+        title,
+        body,
+        createdAt: new Date().toLocaleDateString("ko", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+      },
+    });
+  };
 
   return (
     <div className="post-form">
       <textarea
         className={`post-form__textarea ${notoSansKr.className}`}
-        required
         id="content"
         name="content"
         placeholder="What is in your mind?"
-        onChange={handleFormTextarea}
-        value={content}
+        onChange={handleBodyText}
+        value={body}
       />
-      <button
-        className={`post-form__button ${notoSansKr.className}`}
-        onClick={handleSubmit}
-      >
-        Write
-      </button>
+      <div className="post-form__button-header">
+        <button
+          className={`post-form__button ${notoSansKr.className}`}
+          onClick={handleSubmit}
+        >
+          Write
+        </button>
+        <input
+          className={`post-form__input-header ${notoSansKr.className}`}
+          id="header"
+          name="header"
+          placeholder="What title is on your mind?"
+          value={title}
+          onChange={handleTitleText}
+        />
+      </div>
     </div>
   );
 };
