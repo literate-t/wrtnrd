f36bc72a6bba319c040c6269df5d65801dd5bc6a
literate <gaekoon@gmail.com>
2024-05-24 11:53:13 +0900

    feat: recoil의 인증 상태를 세션 스토리지에 저장하는 로직을 추가한다

--- src/atoms/authAtoms.ts
+++ src/atoms/authAtoms.ts
@@ -1,11 +1,43 @@
-import { atom } from "recoil";
+import { atom, DefaultValue } from "recoil";
 
 export interface AuthUser {
   id: number;
   email: string;
 }
 
+interface SessionStorageEffectParams<T> {
+  setSelf: (value: T | DefaultValue) => void;
+  onSet: (
+    callback: (
+      newValue: T,
+      oldValue: T | DefaultValue,
+      isReset: boolean
+    ) => void
+  ) => void;
+}
+
+const sessionStorageEffect =
+  <T>(key: string) =>
+  ({ setSelf, onSet }: SessionStorageEffectParams<T>) => {
+    if (typeof window === "undefined") {
+      return;
+    }
+    const savedSessionValue = sessionStorage.getItem(key);
+    if (savedSessionValue) {
+      setSelf(JSON.parse(savedSessionValue));
+    }
+
+    onSet((newValue, _, isReset) => {
+      if (newValue == null || isReset) {
+        sessionStorage.removeItem(key);
+      } else {
+        sessionStorage.setItem(key, JSON.stringify(newValue));
+      }
+    });
+  };
+
 export const authAtoms = atom<AuthUser | null>({
   key: "authAtoms",
   default: null,
+  effects_UNSTABLE: [sessionStorageEffect("authAtoms")],
 });
