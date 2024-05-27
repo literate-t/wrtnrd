5189efbfde69e9be0bcc85714427fe2cdbed60d0
literate <gaekoon@gmail.com>
2024-05-27 22:29:55 +0900

    feat: throttle, debounce 함수를 구현한다 - lodash 라이브러리를 써도 되지만 다시 개념을 다질 겸 직접 작성한다

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -11,3 +11,36 @@ export const getStatus = (error: any): number => {
 
   return status;
 };
+
+export const throttle = (limit: number, callback: Function) => {
+  let lastTime: number = 0;
+  let lastFuncId: any;
+
+  return (args: any) => {
+    const now = Date.now();
+    const timeSinceLastRun = now - lastTime;
+    if (limit <= timeSinceLastRun) {
+      callback(args);
+      lastTime = now;
+    } else {
+      lastFuncId = setTimeout(() => {
+        callback(args);
+        lastTime = now;
+        clearTimeout(lastFuncId);
+        lastFuncId = undefined;
+      }, limit - timeSinceLastRun);
+    }
+  };
+};
+
+export const debounce = (callback: (arg: string) => void, wait: number) => {
+  let timerId: any;
+
+  return (arg: any) => {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      callback(arg);
+    }, wait);
+  };
+};
