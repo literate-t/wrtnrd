ce4fc1bcb02cb97726b440f7da58b0ad006d8456
literate <gaekoon@gmail.com>
2024-05-29 19:35:13 +0900

    feat: 마이 페이지의 컴포넌트를 추가한다

--- src/components/mypage/Author.tsx
+++ src/components/mypage/Author.tsx
@@ -1,5 +1,115 @@
+import "react-toastify/dist/ReactToastify.css";
+import { useFormik } from "formik";
+import { ChangeEvent, useCallback, useState } from "react";
+import { debounce, getDataFromError, notify } from "@/utils/common";
+import axios from "@/utils/axios";
+import { useRecoilValue } from "recoil";
+import { authAtoms } from "@/atoms/authAtoms";
+
+// useFormik 사용해보기
+
+interface FormValue {
+  author: string;
+}
+
+const validate = (values: FormValue): any => {
+  const errors: any = {};
+  if (!values.author) {
+    errors.author = "Required";
+  } else if (values.author.length <= 4) {
+    errors.author = "Must be at least 4 characters";
+  }
+
+  return errors;
+};
+
 const Author = () => {
-  return <div>작가 이름 변경</div>;
+  const [isAvailable, setIsAvailable] = useState<boolean>(false);
+  const authState = useRecoilValue(authAtoms);
+  const formik = useFormik({
+    initialValues: {
+      author: "",
+    },
+    validate,
+    onSubmit: async (values) => {
+      await handleSubmit(values);
+    },
+  });
+
+  const handleSubmit = async (value: FormValue): Promise<void> => {
+    try {
+      const result = await axios("/api/user/change-author", {
+        method: "POST",
+        data: {
+          userId: authState?.id,
+          author: value.author,
+        },
+      });
+      notify(result.data);
+    } catch (e) {
+      notify(getDataFromError(e));
+    }
+
+    await checkAuthorDuplicated(value.author);
+  };
+
+  const checkAuthorDuplicated = async (value: string) => {
+    if (value.length <= 4) {
+      return;
+    }
+
+    // TODO 중복 확인 요청
+    try {
+      const result = await axios("/api/user/check-author-duplicated", {
+        method: "POST",
+        data: {
+          author: value,
+        },
+      });
+      setIsAvailable(true);
+    } catch (e) {
+      setIsAvailable(false);
+    }
+  };
+
+  const debounceCheckDuplicated = useCallback(
+    debounce((value: string) => {
+      checkAuthorDuplicated(value);
+    }, 1200),
+    []
+  );
+
+  const handleAuthor = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    formik.setFieldValue(name, value);
+    debounceCheckDuplicated(value);
+  };
+
+  return (
+    <form onSubmit={formik.handleSubmit} className="mypage-home">
+      <label htmlFor="author-name" className="mypage-home__label">
+        New author name
+      </label>
+      <input
+        id="author-name"
+        name="author"
+        className="mypage-home__input"
+        onChange={handleAuthor}
+        value={formik.values.author}
+      />
+
+      {formik.errors.author && (
+        <div className="mypage-home__error">{formik.errors.author}</div>
+      )}
+      <button
+        type="submit"
+        className={`mypage-home__submit ${isAvailable ? null : "mypage-home__submit--disabled"}`}
+        disabled={!isAvailable}
+      >
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default Author;
--- src/components/mypage/Password.tsx
+++ src/components/mypage/Password.tsx
@@ -1,5 +1,161 @@
+import * as Yup from "yup";
+import { Field, Form, Formik } from "formik";
+import axios from "@/utils/axios";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+import { ChangeEvent, useCallback, useState } from "react";
+import { debounce } from "@/utils/common";
+
+interface FormValue {
+  oldPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
+}
+
+const initValues: FormValue = {
+  oldPassword: "",
+  newPassword: "",
+  confirmNewPassword: "",
+};
+
+const validationScheme = Yup.object({
+  oldPassword: Yup.string(),
+  newPassword: Yup.string()
+    .min(8, "At least 8 characters")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      "Password must contain at least one special character"
+    ),
+  confirmNewPassword: Yup.string().oneOf(
+    [Yup.ref("newPassword")],
+    "Not matched"
+  ),
+});
+
+const notify = (message: string) => toast(message);
+
 const Password = () => {
-  return <div>패스워드 변경</div>;
+  const [oldPassword, setOldPassword] = useState<string>("");
+  const [isPasswordCorrect, setIsPasswordCorrect] = useState<boolean>(false);
+
+  const checkPassword = async (value: string) => {
+    try {
+      await axios("/api/user/check-password", {
+        method: "POST",
+        data: {
+          password: value,
+        },
+      });
+      setIsPasswordCorrect(true);
+    } catch (e) {
+      setIsPasswordCorrect(false);
+    }
+  };
+
+  const handleSubmit = async (values: FormValue) => {
+    let result;
+    try {
+      result = await axios("/api/user/change-password", {
+        method: "POST",
+        data: {
+          newPassword: values.newPassword,
+        },
+      });
+
+      notify(result.data);
+      // TODO: signout 이후 /signin으로 이동
+    } catch (e: any) {
+      const {
+        response: { data },
+      } = e;
+      notify(data);
+    }
+
+    setOldPassword("");
+    values.confirmNewPassword = "";
+    values.newPassword = "";
+  };
+
+  const debounceCheckPassword = useCallback(
+    debounce((value) => {
+      checkPassword(value);
+    }, 1200),
+    []
+  );
+  const handleOldPassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setOldPassword(event.target.value);
+    debounceCheckPassword(event.target.value);
+  };
+
+  return (
+    <Formik
+      initialValues={initValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationScheme}
+    >
+      {({ errors, touched }) => (
+        <Form className="mypage-home">
+          <label htmlFor="old-password" className="mypage-home__label">
+            Old password
+          </label>
+          <Field
+            type="password"
+            id="old-password"
+            name="oldPassword"
+            className="mypage-home__input"
+            onChange={handleOldPassword}
+            value={oldPassword}
+          />
+          {errors.oldPassword && touched.oldPassword ? (
+            <div className="mypage-home__error">{errors.oldPassword}</div>
+          ) : null}
+          {isPasswordCorrect ? (
+            <div className="mypage-home__correct-password">
+              Correct password
+            </div>
+          ) : oldPassword ? (
+            <div className="mypage-home__wrong-password">
+              Check again password
+            </div>
+          ) : null}
+          <label htmlFor="new-password" className="mypage-home__label">
+            New password
+          </label>
+          <Field
+            type="password"
+            id="new-password"
+            name="newPassword"
+            className="mypage-home__input"
+          />
+          {errors.newPassword && touched.newPassword ? (
+            <div className="mypage-home__error">{errors.newPassword}</div>
+          ) : null}
+          <label htmlFor="confirm-new-password" className="mypage-home__label">
+            Confirm new password
+          </label>
+          <Field
+            type="password"
+            id="confirm-new-password"
+            name="confirmNewPassword"
+            className="mypage-home__input"
+          />
+          {errors.confirmNewPassword && touched.confirmNewPassword ? (
+            <div className="mypage-home__error">
+              {errors.confirmNewPassword}
+            </div>
+          ) : null}
+          <button
+            type="submit"
+            className={`mypage-home__submit ${isPasswordCorrect ? null : "mypage-home__submit--disabled"}`}
+            disabled={!isPasswordCorrect}
+          >
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default Password;
