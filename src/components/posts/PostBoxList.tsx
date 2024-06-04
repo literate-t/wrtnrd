a85b73ecc244108304c8c402a5266d595b3004ef
literate <gaekoon@gmail.com>
2024-06-04 14:14:58 +0900

    feat: 무한 스크롤 기능을 추가한다
    - react-query의 useInfiniteQuery를 활용한다
    - useIntersectionObserver.ts로 페이지의 하단을 감지한다
    - fetchNextPage 함수는 리렌더링마다 재생성되지 않아도 되기 때문에 useCallback 훅으로 메모이제이션한다
    - getNextPageParam에서 nextPage 필드는 백엔드에서 해당 정보를 보내준다

--- src/components/posts/PostBoxList.tsx
+++ src/components/posts/PostBoxList.tsx
@@ -1,26 +1,67 @@
 "use client";
 
-import PostBox from "@/components/posts/PostBox";
 import axios from "@/utils/axios";
-import { useQuery } from "@tanstack/react-query";
-import { PostProps } from "@/interfaces/postInterface";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useRecoilValue } from "recoil";
 import { authAtoms } from "@/atoms/authAtoms";
+import React, { useCallback, useEffect, useRef } from "react";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+import { PostProps } from "@/interfaces/postInterface";
+import PostBox from "@/components/posts/PostBox";
 
-const fetchPosts = async (userId: number | undefined) => {
-  const res = await axios.post("/api/post/list", {
-    userId,
+const createDynamicUrlWithObject = (...args: any[]): string => {
+  const urlSearchParams = new URLSearchParams();
+  args.forEach((arg) => {
+    Object.keys(arg).forEach((key) => {
+      if (arg[key] === undefined) {
+        return;
+      }
+      urlSearchParams.set(key, arg[key]);
+    });
   });
+
+  return urlSearchParams.toString();
+};
+
+const fetchPosts = async (page: number, userId: number | undefined) => {
+  const params = {
+    page,
+    userId,
+  };
+  const queryString = createDynamicUrlWithObject(params);
+  const res = await axios.get(`/api/post/list?${queryString}`);
 
   return res.data;
 };
 
 const PostBoxList = () => {
   const authState = useRecoilValue(authAtoms);
-  const { isFetching, isError, data } = useQuery<PostProps[]>({
+  const ref = useRef<HTMLDivElement>(null);
+  const elementEntry = useIntersectionObserver(ref, {});
+  const isPageEnd = !!elementEntry?.isIntersecting;
+
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: ["posts"],
-    queryFn: () => fetchPosts(authState?.id),
+    queryFn: ({ pageParam }) => fetchPosts(pageParam, authState?.id),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
   });
+
+  const fetchNext = useCallback(async () => {
+    const res = await fetchNextPage();
+    if (res.isError) {
+      console.error("fetchNextPage Error", res.error);
+    }
+  }, [fetchNextPage]);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined;
+    if (isPageEnd && hasNextPage) {
+      timerId = setTimeout(fetchNext, 100);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [fetchNext, hasNextPage, isPageEnd]);
 
   // TODO position: fixed 속성의 컴포넌트를 만들어서 Loading 화면을 표현한다
   // if (isFetching) {
@@ -30,9 +71,17 @@ const PostBoxList = () => {
   // if (isError) {
   //   return <div>Error</div>;
   // }
-
   return (
-    <div>{data?.map((post) => <PostBox post={post} key={post.id} />)}</div>
+    <div className="posts">
+      {data?.pages?.map((item, index) => (
+        <React.Fragment key={index}>
+          {item.data.map((post: PostProps) => (
+            <PostBox post={post} key={post.id} />
+          ))}
+        </React.Fragment>
+      ))}
+      <div className="posts__bar" ref={ref}></div>
+    </div>
   );
 };
 
