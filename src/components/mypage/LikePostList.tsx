04049c956c26a8d7608e7e350db39e12fa9c0c56
literate <gaekoon@gmail.com>
2024-06-07 19:43:22 +0900

    feat: 좋아요 포스트를 모아서 볼 수 있다
    - PostBoxList와 다른 부분은 서버에서 받은 실제 post 데이터를 state로 관리한다
     - 좋아요를 취소했을 때 바로 리렌더링을 할 수 있다
     - 이 로직을 handleLikeClick에 추가한다
     - 좋아요를 취소한 포스트는 메인에서도 좋아요가 취소된다

--- src/components/mypage/LikePostList.tsx
+++ src/components/mypage/LikePostList.tsx
@@ -1,5 +1,75 @@
+"use client";
+
+import { useRecoilValue } from "recoil";
+import { authAtoms } from "@/atoms/authAtoms";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { fetchPosts } from "@/utils/common";
+import { PostProps } from "@/interfaces/postInterface";
+import PostBox from "@/components/posts/PostBox";
+
 const LikePostList = () => {
-  return <div>like post</div>;
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const authState = useRecoilValue(authAtoms);
+  const ref = useRef<HTMLDivElement>(null);
+  const elementEntry = useIntersectionObserver(ref, {});
+  const isPageEnd = !!elementEntry?.isIntersecting;
+
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ["likePosts"],
+    queryFn: ({ pageParam }) =>
+      fetchPosts("/api/post/like-list", {
+        page: pageParam,
+        userId: authState?.id,
+      }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  });
+
+  const fetchNext = useCallback(async () => {
+    const res = await fetchNextPage();
+    if (res.isError) {
+      console.error("fetchNextPage Error", res.error);
+    }
+  }, [fetchNextPage]);
+
+  const handleLikeClick = (id: number | undefined) => {
+    if (undefined === id) {
+      return;
+    }
+
+    setPosts((prev) => prev.filter((post) => post.id !== id));
+  };
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined;
+    if (isPageEnd && hasNextPage) {
+      timerId = setTimeout(fetchNext, 100);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [fetchNext, hasNextPage, isPageEnd]);
+
+  useEffect(() => {
+    const postArr: PostProps[] = [];
+    data?.pages?.map((item) => {
+      item.data?.map((post: PostProps) => {
+        postArr.push(post);
+      });
+    });
+
+    setPosts(postArr);
+  }, [data]);
+
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <PostBox post={post} key={post.id} onLikeClick={handleLikeClick} />
+      ))}
+      <div className="posts__bar" ref={ref}></div>
+    </div>
+  );
 };
 
 export default LikePostList;
