0c903425407febd2bae817f9ca85596425bec1b4
literate <gaekoon@gmail.com>
2024-06-17 18:30:47 +0900

    fix: 배포 관련 트러블슈팅 중

--- src/hooks/useAuthInterceptor.tsx
+++ src/hooks/useAuthInterceptor.tsx
@@ -4,9 +4,9 @@ import { useEffect } from "react";
 import axios from "@/utils/axios";
 import { useRecoilValue } from "recoil";
 import { authAtoms } from "@/atoms/authAtoms";
-import { getStatus, notify } from "@/utils/common";
+import { getStatus } from "@/utils/common";
 import { useAuth } from "@/providers/AuthProvider";
-import { ERROR_UNAUTHORIZED_401, SIGN_IN_FAILURE } from "@/utils/constants";
+import { ERROR_UNAUTHORIZED_401 } from "@/utils/constants";
 import { useRouter } from "next/navigation";
 import { SIGN_URL } from "@/utils/urls";
 
@@ -24,10 +24,10 @@ const useAuthInterceptor = () => {
         const status = getStatus(error);
 
         if (ERROR_UNAUTHORIZED_401 == status) {
-          if (null != authState) {
-            signOut();
-          }
-          notify(SIGN_IN_FAILURE);
+          // if (null != authState) {
+          //   signOut();
+          // }
+          // notify(SIGN_IN_FAILURE);
           router.push(SIGN_URL);
 
           return Promise.reject(null);
