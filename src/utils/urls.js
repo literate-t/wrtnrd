b1b02a504b307adeb5766b584952689bd3e9eba0
literate <gaekoon@gmail.com>
2024-06-21 21:22:20 +0900

    feat: 상수로 사용할 url을 추가한다

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,8 @@
 // Frontend
 export const ROOT_URL = "/";
 export const SIGN_URL = "/signin";
+export const LOGOUT_URL = "/api/auth/logout";
+export const NEW_TOKEN_URL = "/api/auth/new-tokens";
 
 // Backend
 const baseURL = "http://localhost:8080";
